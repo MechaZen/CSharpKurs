5ae514e6c59eab97e450b94897570e4c96167e02
Michal Kamyk <m.kamyk@laposte.net>
2018-05-01 20:39:06 +0200

    Pętle i parsowanie.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,14 +6,30 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            Console.WriteLine("Podaj swoje imię: ");
+            string name = Console.ReadLine();
+
             Console.WriteLine("Witaj, " + name);
+
+            Console.WriteLine("Podaj swój wiek: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age > 18)
+            {
+                Console.WriteLine("Jesteś pełnoletni");
+            }
+            else
+            {
+                Console.WriteLine("Jesteś niepełnoletni");
+            }
+
+            Console.ReadKey();
+
 
             //foreach (var item in args)
             //{
             //    Console.WriteLine("Witaj, " + item);
             //}
         }
-    
     }
 }
